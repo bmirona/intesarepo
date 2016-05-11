986ca036a01f55d800f43472169cf64f2a8ecf23
bmirona <b.mirona@yahoo.com>
2016-05-11 17:24:53 +0200

    delete some files

--- pgconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbconn = pg_connect("host=ec2-54-228-219-2.eu-west-1.compute.amazonaws.com port=5432 dbname=d33bqbai8cac73 user=everrsslsbdsjo password=wEttOMA42PlEuhmQmdwSOtWZuI");
-
-if (!$dbconn) {
-
-
-    die("Error in connection: " . pg_last_error());
-}
