8d1cd787c5577c0d84e751cedc7de6bedf1b41be
bmirona <b.mirona@yahoo.com>
2016-05-11 13:02:09 +0200

    testing database connection

--- connect.php
+++ connect.php
@@ -1,7 +1,11 @@
 <?php
 try {
   $dbh = new PDO("pgsql:host=ec2-54-228-219-2.eu-west-1.compute.amazonaws.com port=5432 dbname=d33bqbai8cac73", "everrsslsbdsjo", "wEttOMA42PlEuhmQmdwSOtWZuI");
-  echo "PDO connection object created";
+  //echo "PDO connection object created";
+  $dbh->prepare("SELECT * FROM event");
+  $dbh->execute();
+  $test=$dbh->fetchAll(PDO::FETCH_ASSOC);
+  print_r($test);
 }
 catch(PDOException $e) {
   echo $e->getMessage();
